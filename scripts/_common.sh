790a1375cdea6e87d701fefd6094816ce2f6b606
ericgaspar <junk.eg@free.fr>
2021-04-08 19:21:42 +0200

    Upgrade Node to 15

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql apt-transport-https"
 
-nodejs_version=14
+nodejs_version=15
 
 #=================================================
 # PERSONAL HELPERS
