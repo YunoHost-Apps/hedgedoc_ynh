cd1359a1738ccfa785a8c3831d0bb1abe0f90127
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-12-03 08:52:18 +0100

    Testing (#35)
    * Upgrade to 1.9.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql apt-transport-https"
 
-nodejs_version=14
+nodejs_version=16
 
 #=================================================
 # PERSONAL HELPERS
