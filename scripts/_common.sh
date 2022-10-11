eefc6250e6f11d08407f233100972c9567d01c00
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-10-11 19:16:32 +0200

    Testing (#45)
    * Small fixes
    
    * 1.7.0 (#4)
    
    * Upgrde to v.1.7.0
    
    * Set requirements to 3.8.1
    
    * Update README.md
    
    * Upgrade to 1.7.1 (#6)
    
    * Add limitations
    
    * Avoid to buid frontend bundle
    
    * Remove PostegrSQL warning
    
    * Update README.md
    
    Add you should reload service for changes in config to be registered.
    
    * Add restart systemd tuto
    
    * Change version
    
    * Finding an available port
    
    * Upgrade to version 1.7.2 (#12)
    
    * Patch (#17)
    
    * Cleaning up
    
    * Update manifest.json
    
    * Cleaning up
    
    * Secure config file
    
    * Remove yarn
    
    * Update change_url
    
    * Update change_url
    
    * Revert "Remove yarn"
    
    This reverts commit a8cabf0c0addf8f9c4569b27ae04512db5c93df0.
    
    * Update change_url
    
    * Update systemd.service
    
    * Update systemd.service
    
    * Cleaning up
    
    * Fix
    
    * fix badges
    
    * Upgrade to version 1.8.0 (#21)
    
    * Upgrade to version 1.8.0
    
    * Upgrade to 1.8.0
    
    * Upgrade to 1.8.0
    
    * Upgrade to version 1.8.1
    
    * [autopatch] Update issue and PR templates (#25)
    
    Co-authored-by: Yunohost-Bot <>
    
    * Add templates
    
    * Upgrade to 1.8.2
    
    * Update check_process
    
    * Add templates
    
    * Auto-update README
    
    * Set new requirements (#28)
    
    * Fix
    
    * Simplify config
    
    * Auto-update README
    
    Co-authored-by: Yunohost-Bot <>
    
    * Update manifest.json
    
    * Auto-update README
    
    * 1.9.0 (#31)
    
    * 1.9.0
    
    * Update manifest.json
    
    * Auto-update README
    
    * Systemd hardening (#33)
    
    * Update systemd.service
    
    * Update app.src
    
    * Update manifest.json
    
    * 1.9.1 (#34)
    
    * Upgrade to 1.9.1
    
    * 1.9.2 (#36)
    
    * Remove replace_string
    
    * Fix (#39)
    
    * Update manifest.json
    
    * Auto-update README
    
    * Update config.json.example
    
    * Update config.json.example
    
    * 1.9.3
    
    * Auto-update README
    
    * 1.9.4 (#42)
    
    * 1.9.4
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Auto-update README
    
    * Update manifest.json
    
    * Update _common.sh
    
    * set relative path for --keep opt
    
    * Update manifest.json
    
    * Auto-update README
    
    Co-authored-by: Eauchat <34686393+eauchat@users.noreply.github.com>
    Co-authored-by: Alexandre Aubin <alex.aubin@mailoo.org>
    Co-authored-by: YunoHost Bot <yunohost-bot@users.noreply.github.com>
    Co-authored-by: Yunohost-Bot <>
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https"
+pkg_dependencies="postgresql"
 
 nodejs_version=16
 
