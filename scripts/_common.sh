11f6df1f574f64368092e360b199abf9b3ebd231
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-27 19:07:27 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="postgresql"
-
 nodejs_version=16
 
 #=================================================
